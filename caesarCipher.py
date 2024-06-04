c420b546a0e6ba4877a14ba6a7ce406955d8740a
adhoomy <150877800+adhoomy@users.noreply.github.com>
2024-06-04 03:53:37 -0400

    added code for caesar cipher

---
+++ caesarCipher.py
@@ -0,0 +1,42 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def encrypt(text, shift):
+    cipher_text = []
+    for letter in text:
+        cipher_text.append(alphabet[(alphabet.index(letter)+shift)%26])
+    print(f"The encoded text is {cipher_text}")
+
+def decrypt(text, shift):
+    decipher_text = []
+    for letter in text:
+        decipher_text.append(alphabet[(alphabet.index(letter)-shift)%26])
+    print(f"The decoded text is {decipher_text}")
+
+def keepGoing(choice):
+    if choice == "yes":
+        return True
+    elif choice == "no":
+        return False
+    else:
+        choice = (input('Would you like to continue encoding or decoding, please type "yes" or "no":\n')).lower()
+        return keepGoing(choice)
+
+go = True
+while go==True:
+    direction = input('Type "encode" to encrypt, type "decode" to decrypt:\n')
+    if direction=="encode":
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number:\n"))
+        encrypt(text, shift)
+        choice = (input('Would you like to continue encoding or decoding, please type "yes" or "no":\n')).lower()
+        go=keepGoing(choice)
+    elif direction=="decode":
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number:\n"))
+        decrypt(text, shift)
+        choice = (input('Would you like to continue encoding or decoding, please type "yes" or "no":\n')).lower()
+        go=keepGoing(choice)
+    else:
+        direction = input('Type "encode" to encrypt, type "decode" to decrypt:\n')
+
+# what to work on: print the encoded and decoded lists as strings
