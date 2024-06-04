05ba8174de505190e67f73b227071ec631112dfa
adhoomy <150877800+adhoomy@users.noreply.github.com>
2024-06-04 14:23:45 -0400

    added logo and allowing of special characters

--- caesarCipher/caesarCipher.py
+++ caesarCipher/caesarCipher.py
@@ -1,22 +1,27 @@
+import art
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def caesar(direction, text, shift):
     textList = []
     if direction=="encode":
         for letter in text:
-            textList.append(alphabet[(alphabet.index(letter)+shift)%26])
-        finalText = str()
-        for letter in textList:
-            finalText += letter
-        print(f"The encoded text is {finalText}")
+            if letter in alphabet:
+                textList.append(alphabet[(alphabet.index(letter)+shift)%26])
+            else:
+                textList.append(letter)
     if direction=="decode":
         for letter in text:
-            textList.append(alphabet[(alphabet.index(letter)-shift)%26])
-        finalText = str()
-        for letter in textList:
-            finalText += letter
-        print(f"The decoded text is {finalText}")
+            if letter in alphabet:
+                textList.append(alphabet[(alphabet.index(letter)-shift)%26])
+            else:
+                textList.append(letter)
+    finalText = str()
+    for letter in textList:
+        finalText += letter
+    print(f"The decoded text is {finalText}")
 
+print(art.logo)
 go = True
 while go==True:
     direction = input('Type "encode" to encrypt, type "decode" to decrypt:\n')
