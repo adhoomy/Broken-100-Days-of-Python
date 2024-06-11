10f3ce615993d4b771f582c7bb6ba4b8c4871c82
adhoomy <150877800+adhoomy@users.noreply.github.com>
2024-06-11 00:30:03 -0400

    added comments to code

--- HigherOrLower/HigherOrLower.py
+++ HigherOrLower/HigherOrLower.py
@@ -2,17 +2,20 @@ import Art, GameData, random
 from replit import clear
 
 score = 0
+# compareA is outside of the loop so we can assign it to the account number from the list with more followers for the next round
 compareA = random.randint(0, len(GameData.data)-1)
 
 streak = True
 while streak == True:
     print(f"{Art.logo}\n")
     
+    # makes accountA the dictionary in the list
     accountA = GameData.data[compareA]
     print(f"Compare A: {accountA['name']}, a {accountA['description']}, from {accountA['country']}.")
     followersA = accountA['follower_count']
     print(f"{Art.vs}\n")
     
+    # makes accountB another account to compare accountA to
     compareB = random.randint(0, len(GameData.data)-1)
     while compareB==compareA:
         compareB = random.randint(0, len(GameData.data)-1)
@@ -20,11 +23,14 @@ while streak == True:
     print(f"Compare B: {accountB['name']}, a {accountB['description']}, from {accountB['country']}.\n")
     followersB = accountB['follower_count']
 
+    # sets the answers to the account with more followers
     if followersA > followersB:
         answer = 'a'
     if followersB > followersA:
         answer = 'b'
 
+    # checks if the guess is valid
+    # compares the guess to the answer and if the streak continues
     guess = input("Who has more followers? type 'A' or 'B': ").lower()
     while guess!='a' and guess!='b':
         guess = input("Please enter a valid input: ").lower()
