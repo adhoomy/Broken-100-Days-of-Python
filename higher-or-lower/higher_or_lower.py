6e57d9a9d9db795f417f88f85925409f7ecfcd87
adhoomy <150877800+adhoomy@users.noreply.github.com>
2024-06-17 19:44:33 -0400

    changed names of files and folders to be the same format

--- higher-or-lower/higher_or_lower.py
+++ higher-or-lower/higher_or_lower.py
@@ -1,25 +1,25 @@
-import Art, GameData, random
+import art, game_data, random
 from replit import clear
 
 score = 0
 # compareA is outside of the loop so we can assign it to the account number from the list with more followers for the next round
-compareA = random.randint(0, len(GameData.data)-1)
+compareA = random.randint(0, len(game_data.data)-1)
 
 streak = True
 while streak == True:
-    print(f"{Art.logo}\n")
+    print(f"{art.logo}\n")
     
     # makes accountA the dictionary in the list
-    accountA = GameData.data[compareA]
+    accountA = game_data.data[compareA]
     print(f"Compare A: {accountA['name']}, a {accountA['description']}, from {accountA['country']}.")
     followersA = accountA['follower_count']
-    print(f"{Art.vs}\n")
+    print(f"{art.vs}\n")
     
     # makes accountB another account to compare accountA to
-    compareB = random.randint(0, len(GameData.data)-1)
-    while compareB==compareA:
-        compareB = random.randint(0, len(GameData.data)-1)
-    accountB = GameData.data[compareB]
+    compareB = random.randint(0, len(game_data.data)-1)
+    while compareB == compareA:
+        compareB = random.randint(0, len(game_data.data)-1)
+    accountB = game_data.data[compareB]
     print(f"Compare B: {accountB['name']}, a {accountB['description']}, from {accountB['country']}.\n")
     followersB = accountB['follower_count']
 
@@ -32,14 +32,14 @@ while streak == True:
     # checks if the guess is valid
     # compares the guess to the answer and if the streak continues
     guess = input("Who has more followers? type 'A' or 'B': ").lower()
-    while guess!='a' and guess!='b':
+    while guess != 'a' and guess != 'b':
         guess = input("Please enter a valid input: ").lower()
-    if guess!=answer:
+    if guess != answer:
         clear()
-        print(Art.logo)
+        print(art.logo)
         print(f"Wrong, your final score is: {score}.")
         streak = False
-    if guess==answer:
+    if guess == answer:
         score += 1
         compareA = compareB
         clear()
