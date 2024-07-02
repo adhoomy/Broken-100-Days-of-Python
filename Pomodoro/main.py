1faadfe7621974bbbaa836188fbf07c11822117b
adhoomy <150877800+adhoomy@users.noreply.github.com>
2024-07-01 23:14:46 -0400

    finished code for pomodoro program

--- Pomodoro/main.py
+++ Pomodoro/main.py
@@ -1,5 +1,6 @@
 from tkinter import *
-# ---------------------------- CONSTANTS ------------------------------- #
+import math
+
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
@@ -8,14 +9,63 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
-# ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    timer_heading.config(text="Timer", fg=GREEN)
+    checks_shown.config(text="")
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+    global reps
+    reps = 0
 
-# ---------------------------- UI SETUP ------------------------------- #
+
+def start_timer():
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        timer_heading.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        timer_heading.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        timer_heading.config(text="Work", fg=GREEN)
+
+
+def count_down(count):
+    global reps
+
+    count_minutes = math.floor(count / 60)
+    count_seconds = count % 60
+    if count_minutes < 10:
+        count_minutes = f"0{count_minutes}"
+    if count_seconds < 10:
+        count_seconds = f"0{count_seconds}"
+    canvas.itemconfig(timer_text, text=f"{count_minutes}:{count_seconds}")
+
+    if count > 0:
+        global timer
+        # 1000 is 1000 milliseconds, which is 1 second
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps / 2)
+        for _ in range(work_sessions):
+            marks += "✓"
+        checks_shown.config(text=marks)
+
+
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
@@ -26,16 +76,16 @@ timer_heading.grid(column=1, row=0)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_image = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_image)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
 canvas.grid(column=1, row=1)
 
-start_button = Button(text="Start", font=(FONT_NAME, 10, "bold"), fg="black")
+start_button = Button(text="Start", font=(FONT_NAME, 10, "bold"), fg="black", command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset", font=(FONT_NAME, 10, "bold"), fg="black")
+reset_button = Button(text="Reset", font=(FONT_NAME, 10, "bold"), fg="black", command=reset_timer)
 reset_button.grid(column=2, row=2)
 
-checks_shown = Label(text="✓", font=(FONT_NAME, 15, "bold"), fg=GREEN, bg=YELLOW)
+checks_shown = Label(font=(FONT_NAME, 15, "bold"), fg=GREEN, bg=YELLOW)
 checks_shown.grid(column=1, row=3)
 
 window.mainloop()
