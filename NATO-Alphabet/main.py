b252e9cd060322c9f6f999e0afc47ddfe84fe6b5
adhoomy <150877800+adhoomy@users.noreply.github.com>
2024-07-04 23:16:18 -0400

    added key error exception to code

--- NATO-Alphabet/main.py
+++ NATO-Alphabet/main.py
@@ -3,6 +3,16 @@ import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_alphabet = {row.letter: row.code for (index, row) in data.iterrows()}
 
-word = input("Enter a word: ").upper()
-encoded_word = [nato_alphabet[letter] for letter in word]
-print(encoded_word)
+
+def encode():
+    word = input("Enter a word: ").upper()
+    try:
+        encoded_word = [nato_alphabet[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        encode()
+    else:
+        print(encoded_word)
+
+
+encode()
