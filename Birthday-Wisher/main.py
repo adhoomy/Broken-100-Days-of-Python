9669926c48a88ae12489afcabc60c7a3a530fedb
adhoomy <150877800+adhoomy@users.noreply.github.com>
2024-07-07 23:36:12 -0400

    added code for birthday wisher

---
+++ Birthday-Wisher/main.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+import pandas
+import random
+import smtplib
+
+today = (datetime.now().month, datetime.now().day)
+MY_EMAIL = "adhoomytesting@gmail.com"
+MY_PASSWORD = "usohtelgukkjsmtl"
+
+data = pandas.read_csv("birthdays.csv")
+birthday_dict = {(data_row.month, data_row.day): data_row for (index, data_row) in data.iterrows()}
+
+if today in birthday_dict:
+    person = birthday_dict[today]
+    letter_template = f"./letter-templates/letter_{random.randint(1, 3)}.txt"
+    with open(letter_template) as file:
+        contents = file.read()
+        new_contents = contents.replace("[NAME]", person["name"])
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL, to_addrs=person["email"],
+                            msg=f"Subject:Happy Birthday!\n\n{new_contents}")
